3369f48b7564a4e6964e50a7330293ce8db96302
JasmineYuan <jyuan@splunk.com>
2016-05-29 20:29:29 +0800

    adjust the project structure due to its requirement change; introduce handlebars

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,29 @@
  * Created by JasmineYuan on 5/25/16.
  */
 $(document).ready(function() {
-    $('#cat').click(function(e) {
+    //cat data
+    var catCollection = [
+        {
+            name: 'DogEgg',
+            pic: 'public/img/dogEgg.jpg'
+        },
+        {
+            name: 'CatEgg',
+            pic: 'public/img/catEgg.jpg'
+        }
+    ];
+
+    //handlebars template
+    var templateScript = $('#cat-group-tpl').html();
+    var template = Handlebars.compile(templateScript);
+
+    //generate html using template and data
+    $('#container').append(template(catCollection));
+
+    //bind click event to each group
+    $('.group img').click(function(e) {
         //add counter when clicking the picture
-        $('#number').text(parseInt($('#number').text()) + 1);
+        var current = $(this).siblings('p').find($('.number'))[0];
+        $(current).text(parseInt($(current).text()) + 1 );
     });
 });
