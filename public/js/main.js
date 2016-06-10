e9fa19a3f1015124e4937054adcdc1b96292f55b
JasmineYuan <jyuan@splunk.com>
2016-06-10 23:00:17 +0800

    Add a sidebar and implement the click-show functionality

--- public/js/main.js
+++ public/js/main.js
@@ -26,17 +26,37 @@ $(document).ready(function() {
         }
     ];
 
-    //handlebars template
-    var templateScript = $('#cat-group-tpl').html();
-    var template = Handlebars.compile(templateScript);
+    //handlebars cat template
+    var catTemplateScript = $('#cat-group-tpl').html();
+    var catTemplate = Handlebars.compile(catTemplateScript);
 
     //generate html using template and data
-    $('#container').append(template(catCollection));
+    $('#cat-container').append(catTemplate(catCollection));
+    
+    //set the first container visible when loaded
+    $($('#cat-container').find('.group')[0]).addClass('show');
+
+    //handle list template
+    var catListScript = $('#cat-list-tpl').html();
+    var catListTemplate = Handlebars.compile(catListScript);
+
+    $('#cat-list ul').append(catListTemplate(catCollection));
 
     //bind click event to each group
     $('.group img').click(function(e) {
         //add counter when clicking the picture
         var current = $(this).siblings('p').find($('.number'))[0];
-        $(current).text(parseInt($(current).text()) + 1 );
+        $(current).text(parseInt($(current).text()) + 1);
+    });
+
+    $('.cat-li').click(function(e) {
+        //add clicked class to this li and remove this class from others
+        $(this).addClass('clicked');
+        $(this).siblings().removeClass('clicked');
+
+        //handle related cat container
+        var related = $(this).find('span').data('related');
+        $('#' + related).addClass('show');
+        $('#' + related).siblings().removeClass('show');
     });
 });
